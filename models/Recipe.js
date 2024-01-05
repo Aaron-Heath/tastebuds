a6d9d3b136b2adc984355c3efb63a16b4678ef66
unknown <anthony_rosati@yahoo.com>
2024-01-04 21:05:36 -0600

    Recipe model

---
+++ models/Recipe.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes, INTEGER } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Recipe extends Model {}
+
+Recipe.init(
+    {
+        recipe_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        creator_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'user_id'
+            }
+        },
+        ingredients: {
+            type: DataTypes.JSON,
+            allowNull: false
+        },
+        directions: {
+            type: DataTypes.JSON,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'recipe'
+    }
+);
+
+module.exports = Recipe;
