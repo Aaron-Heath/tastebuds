df8021f01918b7b90990c5972f09ec460e733e42
aheath <aaron.heath5447@gmail.com>
2024-01-05 08:28:44 -0500

    Debug model associations. index. now manages many to many relationships

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,35 +1,44 @@
 const router = require('express').Router();
-const { User,CookBook } = require('../../models/User');
+const { User , Cookbook } = require('../../models');
 
 
 router.get('/signup', async (req,res) => {
-    console.log(req.method + "request for " + req.path);
+    console.log(req.method + " request for " + req.path);
     res.json({message:"signup request heard"});
 }); 
 
 router.post('/signup', async (req,res) => {
+    console.log("Post request heard")
+    // console.log(req.body);
     try {
         // Create user in db
+        console.log("Creating user");
         const userData = await User.create(req.body);
-        req.session.user_id = userData.id;
+        // console.log(userData);
 
+
+        // req.session.user_id = userData.id;
         // Create default private cookbook
-        const cookBookData = await CookBook.create(
+        console.log("Creating cookbook");
+        const cookBookData = await Cookbook.create(
             {
                 creator_id: userData.id,
                 title: "My Cookbook",
                 isPublic: false,
-                desciption: "This is my default cookbook!",
+                description: "This is my default cookbook!",
                 viewers: null,
                 editors: null,
                 recipes: null,
             }
         );
+        console.log(cookBookData);
 
         //TODO - Send verification email
         
-        // Redirect to login
-        res.redirect('/login');
+        // TODO - Redirect to login
+        res.json({
+            message: "Success!"
+        });
 
     } catch (err) {
         res.status(400).json(err);
