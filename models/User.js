08ce7d4852fbe756aa169c54d0825e7cf9546166
aheath <aaron.heath5447@gmail.com>
2024-01-05 15:05:49 -0500

    Add firstName and lastName to user model

--- models/User.js
+++ models/User.js
@@ -16,6 +16,14 @@ User.init(
         primaryKey: true,
         autoIncrement: true,
       },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
       username: {
         type: DataTypes.STRING,
         unique: true,
