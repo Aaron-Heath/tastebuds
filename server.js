30ece3fdc40c1b4e4826edf150550cf04c4c2700
aheath <aaron.heath5447@gmail.com>
2024-01-05 23:14:59 -0500

    Install express-session and connect-session-sequelize

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -10,10 +13,27 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
 
+const sess = {
+    secret: process.env.SESSION_SALT,
+    cookie: {
+      maxAge: 600000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // Set middleware
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
