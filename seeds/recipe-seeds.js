cf76212dc230bf4e56f8e3e5ac6b29dc689f9eb1
unknown <anthony_rosati@yahoo.com>
2024-01-06 22:41:11 -0600

    Added code to recipe-seeds.js

---
+++ seeds/recipe-seeds.js
@@ -0,0 +1,31 @@
+const { Recipe } = require('../models');
+
+const recipeData = [
+    {
+        creator_id: 2,
+        ingredients: { '1': 'Frozen pizza'},
+        directions: { 
+            'Step One': 'Preheat oven to 500 degrees.',
+            'Step Two': 'Insert unwrapped frozen pizza into oven and cook for recommended time.',
+            'Step Three': 'Remove from oven when time is up, cut, and eat!'
+        }
+    },
+    {
+        creator_id: 1,
+        ingredients: { 
+            '1': '2 pounds of lasagna noodles',
+            '2': 'A quart of tomato sauce',
+            '3': 'A pound of shredded cheese'
+        },
+        directions: {
+            'Step One': 'Preheat oven to 500 degrees.',
+            'Step Two': 'In a baking dish, layer noodles, sauce, and cheese, leaving a half inch of room from the top of the dish.',
+            'Step Three': 'Place dish into oven and back for 40 minutes - 1 hour.',
+            'Step Four': 'Remove dish from oven and allow to set for 15 minutes before serving.'
+        }
+    }
+]
+
+const seedRecipes = () => Recipe.bulkCreate(recipeData);
+
+module.exports = seedRecipes;
