227e9e2556c97e9e4eb56d9a4fae047cbf8bd279
unknown <anthony_rosati@yahoo.com>
2024-01-09 17:53:02 -0600

    Created PUT method in public/js/recipe.js

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -6,7 +6,8 @@ const newRecipe = async () => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
-            },   
+            },
+            body: JSON.stringify(),   
         });
 
         if (response.ok) {
@@ -16,5 +17,30 @@ const newRecipe = async () => {
         }
     } catch (err) {
         console.error(err)
+    };
+};
+
+// Put method for updating recipes
+// TODO: Figure out recipe_id variable
+// TODO: Create query selector for element to pull input from
+const updateRecipe = async () => {
+    try {
+        const response = await fetch(`/api/recipe/${recipe}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Successful PUT requrest!');
+            return data;
+        }
+
+
+    } catch (err) {
+        console.error(err);
     };
 };
