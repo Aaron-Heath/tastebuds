cb4f98032c9b6f64c06e149572353e2760fe23f3
unknown <anthony_rosati@yahoo.com>
2024-01-09 19:53:22 -0600

    Created get route to include send all cookbook data

--- controllers/recipeRoutes.js
+++ controllers/recipeRoutes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
+const { Cookbook } = require('../models')
 
 // The /recipe route
 router.get('/', async (req, res) => {
-    res.render('recipe');
+
+    const dbCookbookData = await Cookbook.findAll();
+
+    const cookbooks = await dbCookbookData.map((cookbook) => 
+    cookbook.get({ plain: true})
+    );
+
+    res.render('recipe', {
+        cookbooks,
+    });
 });
 
 module.exports = router;
