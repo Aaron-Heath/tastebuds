1739c5ba61ecd9ccb3646a593a94888967dec259
aheath <aaron.heath5447@gmail.com>
2024-01-09 20:02:32 -0500

    Update signup.js to render prompt after signup. Update signup-login.css

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,4 +1,5 @@
 const form = document.querySelector('.form');
+const formContainer = document.querySelector('.form-container');
 
 async function submitForm(event) {
     event.preventDefault(); // Prevents the default form submission
@@ -33,20 +34,42 @@ async function submitForm(event) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
-        const data = await response.json();
-        // Handles the response data
-        console.log(data);
-
-        // TODO: prompt user to check their email
-
-        //clear innerHtml on form-container
-
-        // create new html elements telling user to check their email and activate their account
-
-        
     } catch (error) {
         console.error('Error:', error);
     }
+        console.log("Request complete");
+
+        // Handles the response data
+
+        // TODO: prompt user to check their email
+        //clear innerHtml on form-container
+        formContainer.innerHTML = "";
+
+        // create new html elements telling user to check their email and activate their account
+        const activationPrompt = document.createElement('div');
+        activationPrompt.id = "activation-prompt";
+
+        // Create add content
+        const activationHeader = document.createElement('h2');
+        activationHeader.textContent = "Thank you for signing up!";
+
+        activationPrompt.appendChild(activationHeader);
+
+        const activationText = document.createElement('p');
+
+        activationText.className="form-subtitle";
+        activationText.innerHTML =  'To activate your account, please check your inbox and spam folder for an activation link from <a href="mailto:noreply.tastebuds@gmail.com">noreply.tastebuds@gmail.com</a>.';
+
+        activationPrompt.appendChild(activationText);
+
+        // Render to screen
+        formContainer.appendChild(activationPrompt);
+
+
+
+
+        
+
 }
 
 form.addEventListener('submit', submitForm);
