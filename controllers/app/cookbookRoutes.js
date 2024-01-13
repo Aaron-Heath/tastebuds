791045d661a2b5aab1218aa300dcd6076c46625a
unknown <anthony_rosati@yahoo.com>
2024-01-12 22:31:01 -0600

    Added path for updating cookbooks in app/cookbookRoutes.js

--- controllers/app/cookbookRoutes.js
+++ controllers/app/cookbookRoutes.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
 const { Cookbook } = require('../../models')
 
+// The /app/cookbook/update/:id endpoint for getting the form to create a new cookbook
+router.get('/update/:id', async (req, res) => {
+    try {
+        const dbCookbookData = await Cookbook.findByPk(req.params.id);
+
+        const cookbook = dbCookbookData.get({ plain: true });
+        console.log(cookbook);
+        
+        res.render('app-cookbook-update', { cookbook });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+})
