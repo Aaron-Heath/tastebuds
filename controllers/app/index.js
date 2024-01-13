d0e74a912c133e842b2d6d4ef8618bde9d4459a7
unknown <anthony_rosati@yahoo.com>
2024-01-12 22:27:21 -0600

    Created handlebars files for creating and updating cookbooks, created routes in controllers appropriately

--- controllers/app/index.js
+++ controllers/app/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const appRoutes = require('./appRoutes');
 const recipeRoutes = require('./recipeRoutes')
+const cookbookRoutes = require('./cookbookRoutes')
 
 router.use('/', appRoutes);
 router.use('/recipe', recipeRoutes)
+router.use('/cookbook', cookbookRoutes)
 
 module.exports = router;
