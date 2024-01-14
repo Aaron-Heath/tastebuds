60a3619b308bed8435247dfdd8b6f4fa315850e7
unknown <anthony_rosati@yahoo.com>
2024-01-13 21:44:22 -0600

    Update to cookbook update and creation code using current creator_id and requiring authentication

--- controllers/app/cookbookRoutes.js
+++ controllers/app/cookbookRoutes.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const { Cookbook, User } = require('../../models');
+const withAuth = require('../../utils/auth');
+router.all('*',withAuth);
 
-// TODO: require login
 // The /app/cookbook endpoint for getting the form to create a new cookbook
 router.get('/', async (req, res) => {
     try {
         // Gets current user ID for later use
         const creator_id = req.session.user.id;
-        
+
         res.render('app-cookbook-create', {creator_id: creator_id});
     } catch (err) {
         console.error(err);
@@ -28,7 +29,10 @@ router.get('/update/:id', async (req, res) => {
         user.get({plain: true}));
         console.log(users)
 
-        res.render('app-cookbook-update', { cookbook, users });
+        // Gets current user ID for later use
+        const creator_id = req.session.user.id;
+
+        res.render('app-cookbook-update', { cookbook, users, creator_id: creator_id });
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
