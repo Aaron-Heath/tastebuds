5097b17d7ab25f3362df52fa64a0797d39087672
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-01-16 18:25:06 -0500

    commented out code in appRoutes so page would render also comment out the utilcount file

--- controllers/app/appRoutes.js
+++ controllers/app/appRoutes.js
@@ -4,76 +4,76 @@ const withAuth = require('../../utils/auth');
 
 const router = require('express').Router();
 
-// router.all('*',withAuth);
-// router.get('/', async (req,res) => {
-//     const cookbookData = await Cookbook.findAll({
-//         where: {
-//             creator_id: req.session.user.id
-//         }});
-//     const cookbooks = cookbookData.map((cookbook) => 
-//         cookbook.get({plain: true}));
+router.all('*',withAuth);
+router.get('/', async (req,res) => {
+    const cookbookData = await Cookbook.findAll({
+        where: {
+            creator_id: req.session.user.id
+        }});
+    const cookbooks = cookbookData.map((cookbook) => 
+        cookbook.get({plain: true}));
 
-//     const userData = await User.findByPk(req.session.user.id);
+    const userData = await User.findByPk(req.session.user.id);
 
-    // const sharedCookbookData = await userData.getCookbooks();
-    // const sharedCookbooks = sharedCookbookData.map(sharedCookbook => sharedCookbook.get());
-    // const user = userData.get();
+    const sharedCookbookData = await userData.getCookbooks();
+    const sharedCookbooks = sharedCookbookData.map(sharedCookbook => sharedCookbook.get());
+    const user = userData.get();
     
-    // // add shared parameter
-    // for(let cookbook of sharedCookbooks) {
-    //     cookbook.shared = true;
-    // }
-
-//     res.render('app-home', { 
-//         cookbooks: cookbooks,
-//         sharedCookbooks: sharedCookbooks,
-//         user: user, 
-//         logged_in: req.session.logged_in });
-// });
-router.all('*', withAuth);
-
-router.get('/', async (req, res) => {
-    try {
-        const creatorId = req.session.user.id;
-
-        // Log to check if counts are obtained correctly
-        const cookbookCount = await getCookbookCount(creatorId);
-        const recipeCount = await getRecipeCount();
-        console.log('Cookbook Count:', cookbookCount);
-        console.log('Recipe Count:', recipeCount);
-
-        const cookbookData = await Cookbook.findAll({
-            where: {
-                creator_id: creatorId
-            }
-        });
-
-        const cookbooks = cookbookData.map((cookbook) =>
-            cookbook.get({ plain: true })
-        );
-
-        const userData = await User.findByPk(creatorId);
-
-        const sharedCookbookData = await userData.getCookbooks();
-        const sharedCookbooks = sharedCookbookData.map((sharedCookbook) =>
-            sharedCookbook.get()
-        );
-
-        const user = userData.get();
-
-        res.render('app-home', {
-            cookbooks: cookbooks,
-            sharedCookbooks: sharedCookbooks,
-            user: user,
-            cookbookCount: cookbookCount,  // Make sure variable names match the template
-            recipeCount: recipeCount,      // Make sure variable names match the template
-            logged_in: req.session.logged_in
-        });
-    } catch (error) {
-        console.error('Error:', error.message);
-        res.status(500).json({ error: 'Failed to retrieve data' });
+    // add shared parameter
+    for(let cookbook of sharedCookbooks) {
+        cookbook.shared = true;
     }
+
+    res.render('app-home', { 
+        cookbooks: cookbooks,
+        sharedCookbooks: sharedCookbooks,
+        user: user, 
+        logged_in: req.session.logged_in });
 });
+// router.all('*', withAuth);
+
+// router.get('/', async (req, res) => {
+//     try {
+//         const creatorId = req.session.user.id;
+
+//         // Log to check if counts are obtained correctly
+//         const cookbookCount = await getCookbookCount(creatorId);
+//         const recipeCount = await getRecipeCount();
+//         console.log('Cookbook Count:', cookbookCount);
+//         console.log('Recipe Count:', recipeCount);
+
+//         const cookbookData = await Cookbook.findAll({
+//             where: {
+//                 creator_id: creatorId
+//             }
+//         });
+
+//         const cookbooks = cookbookData.map((cookbook) =>
+//             cookbook.get({ plain: true })
+//         );
+
+//         const userData = await User.findByPk(creatorId);
+
+//         const sharedCookbookData = await userData.getCookbooks();
+//         const sharedCookbooks = sharedCookbookData.map((sharedCookbook) =>
+//             sharedCookbook.get()
+//         );
+
+//         const user = userData.get();
+
+//         res.render('app-home', {
+//             cookbooks: cookbooks,
+//             sharedCookbooks: sharedCookbooks,
+//             user: user,
+//             cookbookCount: cookbookCount,  // Make sure variable names match the template
+//             recipeCount: recipeCount,      // Make sure variable names match the template
+//             logged_in: req.session.logged_in
+//         });
+//     } catch (error) {
+//         console.error('Error:', error.message);
+//         res.status(500).json({ error: 'Failed to retrieve data' });
+//     }
+// });
 
 
 
