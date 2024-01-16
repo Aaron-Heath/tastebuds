ff3e0e38689ff3438c1a106ea990f2fe1937f7cc
aheath <aaron.heath5447@gmail.com>
2024-01-16 15:49:42 -0500

    Fix /login redirect

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,7 @@ router.get('/login', async (req,res) => {
     // If already logged in
     // Redirect to application main page
     if(req.session.logged_in) {
-        res.redirect('/app', { logged_in: req.session.logged_in });
+        res.redirect('/app');
     }
 
     // else serve login page
