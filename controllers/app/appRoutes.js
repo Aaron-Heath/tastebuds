b79e0837a127553b990b673369d8559fdc1e553c
BarkleyRhoat <barkleyrhoat@gmail.com>
2024-01-16 19:24:06 -0500

    deleted utilitycount

--- controllers/app/appRoutes.js
+++ controllers/app/appRoutes.js
@@ -39,52 +39,6 @@ router.get('/', async (req,res) => {
         cookbookCount: cookbookCount,
         logged_in: req.session.logged_in });
 });
-// router.all('*', withAuth);
-
-// router.get('/', async (req, res) => {
-//     try {
-//         const creatorId = req.session.user.id;
-
-//         // Log to check if counts are obtained correctly
-//         const cookbookCount = await getCookbookCount(creatorId);
-//         const recipeCount = await getRecipeCount();
-//         console.log('Cookbook Count:', cookbookCount);
-//         console.log('Recipe Count:', recipeCount);
-
-//         const cookbookData = await Cookbook.findAll({
-//             where: {
-//                 creator_id: creatorId
-//             }
-//         });
-
-//         const cookbooks = cookbookData.map((cookbook) =>
-//             cookbook.get({ plain: true })
-//         );
-
-//         const userData = await User.findByPk(creatorId);
-
-//         const sharedCookbookData = await userData.getCookbooks();
-//         const sharedCookbooks = sharedCookbookData.map((sharedCookbook) =>
-//             sharedCookbook.get()
-//         );
-
-//         const user = userData.get();
-
-//         res.render('app-home', {
-//             cookbooks: cookbooks,
-//             sharedCookbooks: sharedCookbooks,
-//             user: user,
-//             cookbookCount: cookbookCount,  // Make sure variable names match the template
-//             recipeCount: recipeCount,      // Make sure variable names match the template
-//             logged_in: req.session.logged_in
-//         });
-//     } catch (error) {
-//         console.error('Error:', error.message);
-//         res.status(500).json({ error: 'Failed to retrieve data' });
-//     }
-// });
-
-
 
 router.get('/public', (req, res) => {
     res.render('app-public', { logged_in: req.session.logged_in });
